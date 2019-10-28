ffb2583b3ee9c575916d12ee65ab161100b76a16
Raeed Rasul <raeed@umich.edu>
2019-10-28 16:49:10 -0400

    Exponent Implementation

--- rpn.py
+++ rpn.py
@@ -8,10 +8,22 @@ def calculate(arg):
 			arg2 = stack.pop()
 			result = arg1 + arg2
 			stack.append(result)
+		elif token == '-':
+			arg1 = stack.pop()
+			arg2 = stack.pop()
+			result = arg2 - arg1
+			stack.append(result)
+		elif token == '^':
+			arg1 = stack.pop()
+			arg2 = stack.pop()
+			result = arg2^arg1
+			stack.append(result)
 		else:
 			stack.append(int(token))
 		
 		print(stack)
+	if len(stack) != 1:
+		raise TypeError("malformed input")
 	return stack.pop()
 
 def main():
