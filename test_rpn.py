6c5162470eb89432ec716cdb4fcde074638f2e39
Raeed Rasul <raeed@umich.edu>
2019-10-28 16:44:53 -0400

    Exponent Test

--- test_rpn.py
+++ test_rpn.py
@@ -5,3 +5,12 @@ class TestBasics(unittest.TestCase):
 	def test_add(self):
 		result = rpn.calculate("1 1 +")
 		self.assertEqual(2, result)
+	def test_sub(self):
+		result = rpn.calculate("5 3 -")
+		self.assertEqual(2, result)
+	def test_badinput(self):
+		with self.assertRaises(TypeError):
+			rpn.calculate("1 2 3 +")
+	def test_exp(self):
+		result = rpn.calculate("2 3 ^")
+		self.assertEqual(8, result)
